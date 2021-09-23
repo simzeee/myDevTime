0f54eec4d521de2749ed9dc99362bca96a3e27af
simzeee <jwsims816@gmail.com>
2021-09-23 15:11:48 -0500

    canvas and makeArt function completed

--- whiteboard.js
+++ whiteboard.js
@@ -8,28 +8,27 @@
 // Write some function collatz, accepts any number, apply the 2 rules,
 // and return the number of steps it takes to get to 1 (to enter the infinite loop)
 
-function collatz(num) {
-  let count = 0;
-  while (num != 1) {
-    if (num % 2 === 0) {
-      num = num/2
-      count ++
-      console.log(num)
-    }
-    else if(num % 2 != 0){
-      num = 3*num + 1
-      count ++
-      console.log(num)
-    }
-  }
-  return count + " FINAL!!"
-}
+// function collatz(num) {
+//   let count = 0;
+//   while (num != 1) {
+//     if (num % 2 === 0) {
+//       num = num/2
+//       count ++
+//       console.log(num)
+//     }
+//     else if(num % 2 != 0){
+//       num = 3*num + 1
+//       count ++
+//       console.log(num)
+//     }
+//   }
+//   return count + " FINAL!!"
+// }
 
 // console.log(collatz(4))
 // console.log(collatz(1))
-console.log(collatz(12))
+// console.log(collatz(12))
 // console.log(collatz(11293874))
-
 
 /*
 let count = 0;
@@ -41,3 +40,21 @@ while (n !== 1){
 
 return count
 */
+
+let makeArt = () => {
+  const tinyart = document.getElementById('tinyart');
+  const context = tinyart.getContext("2d");
+
+  for (let x = 0; x<255; x++){
+    for(let y = 0; y<255; y++){
+      if((x * y) % 255){
+        context.fillRect(x*4, y*4, 4, 4)
+        context.fillStyle = `rgb(${(x * y) % 255}, ${(x * y) % 255}, ${(x * y) % 255})`
+      }
+    }
+  }
+};
+
+makeArt()
+
+
