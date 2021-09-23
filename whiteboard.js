e7587a7944e0d0306ac3c62fdc58e9cdd0bdd442
simzeee <jwsims816@gmail.com>
2021-09-23 13:33:35 -0500

    collatz completed

--- whiteboard.js
+++ whiteboard.js
@@ -14,15 +14,30 @@ function collatz(num) {
     if (num % 2 === 0) {
       num = num/2
       count ++
+      console.log(num)
     }
     else if(num % 2 != 0){
       num = 3*num + 1
       count ++
+      console.log(num)
     }
   }
-  return count
+  return count + " FINAL!!"
 }
 
-console.log(collatz(4))
-console.log(collatz(1))
+// console.log(collatz(4))
+// console.log(collatz(1))
 console.log(collatz(12))
+// console.log(collatz(11293874))
+
+
+/*
+let count = 0;
+
+while (n !== 1){
+  n % 2 === 0 ? n = n / 2 : n = 3 * n + 1
+  count++
+}
+
+return count
+*/
